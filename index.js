515c55262513b2bd69240b8803530b5601e2b27d
Aniket Sharma <aaniket1999@gmail.com>
2021-06-06 23:19:53 +0530

    Final Stock Web Application Commit

--- index.js
+++ index.js
@@ -4,16 +4,50 @@ const express = require('express');
 const app = express();
 var exphbs  = require('express-handlebars');
 const path = require('path');
+const request =require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+// Use Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+// API key pk_3cc73cbaca404cf19d1fa4559f5f1671
+
+// call create api
+function call_api(finishedAPI, ticker){
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_3cc73cbaca404cf19d1fa4559f5f1671',{json: true}, (err,res, body)=>{
+        if(err){return console.log(err);}
+        if(res.statusCode === 200){
+            // console.log(body);
+            finishedAPI(body);
+        };
+    });
+};
 
 // Set handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// Set handlebars routes
+// Set handlebars index GET routes
 app.get('/', function (req, res) {
-    res.render('home');
+    call_api(function(doneAPI){
+        res.render('home',{
+            stock: doneAPI
+        });
+    });
+});
+
+// Set handlebars index POST routes
+app.post('/', function (req, res) {
+    call_api(function(doneAPI){
+        // posted_stuff = req.body.stock_ticker;
+        res.render('home',{
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
 });
 
 // Create about page route
